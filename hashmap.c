c3e4457f4f4773c27de0b110753d1b55e7aff7ee
Javier Borquez <163423384+JavierBor@users.noreply.github.com>
2024-04-23 13:44:56 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -72,10 +72,8 @@ void eraseMap(HashMap * map,  char * key) {
 Pair * searchMap(HashMap * map,  char * key) {
   long pos;
   if(map==NULL || key==NULL) return NULL;
+  
   pos = hash(key,map->capacity);
-  while(map->buckets[pos]!=NULL && map->buckets[pos]->key!=NULL){
-    pos = (pos+1)%map->capacity;
-  }
   return map->buckets[pos];
 }
 
