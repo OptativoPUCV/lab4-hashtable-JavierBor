d2351f7b9250031c366f5e9bc520af3fb3fb0c26
Javier Borquez <163423384+JavierBor@users.noreply.github.com>
2024-04-23 14:26:56 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -50,9 +50,10 @@ void insertMap(HashMap * map, char * key, void * value) {
 }
 
 void enlarge(HashMap * map) {
-    enlarge_called = 1; //no borrar (testing purposes)
-
-
+  enlarge_called = 1; //no borrar (testing purposes)
+  if(map==NULL) return;
+  map->buckets = (Pair **)realloc(map->buckets,sizeof(Pair *)*map->capacity*2);
+  (map->capacity)++;
 }
 
 
