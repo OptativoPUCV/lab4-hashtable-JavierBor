8f06678682d905bc82d6cd08ff45b768b94da433
Javier Borquez <163423384+JavierBor@users.noreply.github.com>
2024-04-23 14:22:39 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -107,7 +107,8 @@ Pair * firstMap(HashMap * map) {
 Pair * nextMap(HashMap * map) {
   if (map == NULL) return NULL;
   if (map->current == -1) return firstMap(map);
-  for (long i = map->current ; i < map->size ; i++){
+  if (map->current == map->size) return NULL;
+  for (long i = map->current + 1 ; i < map->size ; i++){
     if (map->buckets[i] != NULL && map->buckets[i]->key != NULL){
       map->current = i;
       return map->buckets[i];
